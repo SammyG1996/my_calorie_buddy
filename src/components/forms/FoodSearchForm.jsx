020853d73333f0edb91d295c8f2b87544266b0d2
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2023-08-24 15:58:09 -0400

    fixed styling

--- src/components/forms/FoodSearchForm.jsx
+++ src/components/forms/FoodSearchForm.jsx
@@ -25,7 +25,7 @@ const FoodSearchForm = () => {
         <div>
             <form onSubmit={handleSubmit}>
             <div className='flex mt-10 w-[80vw] md:w-[60vw]'>
-                <input autoComplete='off' placeholder='Enter Food Along With Serving Size' value={inputData} onChange={handleChange} type="text" name="search" id="search" className='w-[100%] py-2 px-2 shadow-md' />
+                <input autoComplete='off' placeholder='Enter Food w/ Serving Size' value={inputData} onChange={handleChange} type="text" name="search" id="search" className='w-[100%] py-2 px-2 shadow-md' />
                 <button type="submit" className='flex items-center px-3 py-2 m-1 bg-cyan-500 hover:bg-cyan-700 shadow-md'>Search</button>
             </div>            
             </form>
