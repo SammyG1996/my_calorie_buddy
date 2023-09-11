26718bf951560985fc2397de3f635ca8ddfe9b4b
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2023-09-11 10:42:46 -0400

    updated alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -11,7 +11,7 @@ const Alert = () => {
     }, [alert])
     return (
         <>
-            {alert && <div className='flex justify-center w-full h-[30px] bg-[#715AFF] mt-[70px]'>
+            {alert && <div className='flex justify-center w-full h-[30px] bg-[#715AFF] mt-[70px] absolute'>
                 <span className='text-[1rem] text-[#F9FCFB]'>{alert}</span>
             </div>}
         </>
